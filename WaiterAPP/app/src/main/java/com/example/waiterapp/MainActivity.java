cbb97923f99ea0d82545f182c580002ae0a6dbcf
Santiago Forero <santifore1104@gmail.com>
2021-10-06 23:07:36 -0500

    Login simple y comienzo firebase

--- WaiterAPP/app/src/main/java/com/example/waiterapp/MainActivity.java
+++ WaiterAPP/app/src/main/java/com/example/waiterapp/MainActivity.java
@@ -2,22 +2,28 @@ package com.example.waiterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private AnimationDrawable anim, anim2;
     private Button button, button2;
+    private EditText email, passw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        init();
+        init(); //conexion con java y xml
 
+        // animacion para los botones
         anim = (AnimationDrawable) button.getBackground();
         anim.setEnterFadeDuration(2300);
         anim.setExitFadeDuration(2300);
@@ -26,13 +32,44 @@ public class MainActivity extends AppCompatActivity {
         anim2.setEnterFadeDuration(2800);
         anim2.setExitFadeDuration(2800);
 
+        //Metodo para boton login ir a activity Cliente y Admin
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if(email.getText().toString().equals("") || passw.getText().toString().equals("")){
+                    Toast.makeText( MainActivity.this, "Ingrese un Email o contraseña", Toast.LENGTH_SHORT).show();
+                }else if(email.getText().toString().equals("dany") && passw.getText().toString().equals("123")){
+                    Intent admin = new Intent(MainActivity.this, Admin.class);
+                    startActivity(admin);
+                    email.getText().clear();
+                    passw.getText().clear();
+                }else if(!email.getText().toString().contains("@")){
+                    Toast.makeText( MainActivity.this, "Le falta el @", Toast.LENGTH_SHORT).show();
+                } else{
+                    Intent cliente = new Intent(MainActivity.this, Cliente.class);
+                    startActivity(cliente);
+                    email.getText().clear();
+                    passw.getText().clear();
+                }
+
+            }
+
+        });
+
+        //conexion firebase
+
     }
 
+    //Conexion de java y xml
     private void init(){
         this.button = findViewById(R.id.button);
         this.button2 = findViewById(R.id.button2);
+        this.email = findViewById(R.id.editTextTextPersonName);
+        this.passw = findViewById(R.id.editTextTextPassword);
     }
 
+    //inicia la animacion de los botones
     @Override
     protected void onResume() {
         super.onResume();
@@ -44,6 +81,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    //detiene la animacion de los botones
     @Override
     protected void onPause() {
         super.onPause();
